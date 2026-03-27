033e1274edd6e839c059ee1d52a1c78743db4f80
mr <muhammad11rasul2011@gmail.com>
2026-03-28 03:39:26 +0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,22 @@
+// js/config.js - O'zingizning Firebase kalitlaringiz
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDAsbqgXxl69P-yhOn3L2puTBmLxG70Ao0",
+    authDomain: "basic-formula-484419-k8.firebaseapp.com",
+    databaseURL: "https://basic-formula-484419-k8-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "basic-formula-484419-k8",
+    storageBucket: "basic-formula-484419-k8.firebasestorage.app",
+    messagingSenderId: "629679926764",
+    appId: "1:629679926764:web:0cff483e9c4752fbd024c9",
+    measurementId: "G-WR9D0BWR86"
+};
+
+export const appConfig = {
+    maxFileSize: 5 * 1024 * 1024,
+    maxMessageLength: 250,
+    defaultZoom: 1,
+    minZoom: 0.2,
+    maxZoom: 5
+};
+
+export default firebaseConfig;
